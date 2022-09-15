5a7d9f260353390ca6162ca4934460d4a7790167
mondousage <casspangell@gmail.com>
2022-09-15 10:51:50 -0600

    Create TapHideKeyboard.swift

---
+++ TapHideKeyboard.swift
@@ -0,0 +1,27 @@
+    /**
+     * Called when 'return' key pressed. return NO to ignore.
+     */
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+       /**
+    * Called when the user click on the view (outside the UITextField).
+    */
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
+      
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
